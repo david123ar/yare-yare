ea9a245e2d206bddf98d471156f0474f7947ac11
rishabh-star <risingmerta@gmail.com>
2024-10-07 15:22:40 +0530

    firstc ommit

--- src/app/api/anime/sitemap/route.js
+++ src/app/api/anime/sitemap/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 const apiUrl = "https://vimalking.vercel.app/api/az-list?page=";
-const baseUrl = "https://animoon.me";
+const baseUrl = "https://kaidox.xyz";
 
 // Helper function for retrying fetch in case of error
 const retryFetch = async (url, retries = 3, delay = 1000) => {
